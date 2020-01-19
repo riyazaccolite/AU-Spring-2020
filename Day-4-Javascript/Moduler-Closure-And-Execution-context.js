61e9b4d391370cd58fb7bfaf92d255065df782d4
Harshit <harshit.chhabra09@gmail.com>
2020-01-19 10:31:18 +0530

    Update

--- Day-4-Javascript/Moduler-Closure-And-Execution-context.js
+++ Day-4-Javascript/Moduler-Closure-And-Execution-context.js
@@ -2,21 +2,22 @@ var myModule = (function(){
     let _registrations = 0;
     let _users = []
 
-    function _updateRegistrations(){ this.registrations++; } //Closure
+    // PRIVATE
+    function _updateRegistrations(){ this.registrations++; } // Closure
 
-    function _User(firstName, lastName, age){
+    function _User(firstName, lastName, age){ // PRIVATE
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
 
-    function _print(){ 
+    function _print(){ // PRIVATE
         for(let key in this)
             console.log(key + ": " + this[key]);
         console.log();
     }
     
-    function _changeUser(firstName, lastName, age){
+    function _changeUser(firstName, lastName, age){ // PRIVATE
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
@@ -25,7 +26,7 @@ var myModule = (function(){
     function addUser(firstName, lastName, age){ 
         let user = new _User(firstName, lastName, age);
 
-        _users.push(user); //CLOSURE
+        _users.push(user); // CLOSURE
 
         _updateRegistrations();
         return user;
@@ -35,11 +36,11 @@ var myModule = (function(){
         _print.call(user); // CALL
     }
 
-    function updateUser(user,userDetails){ //APPLY
+    function updateUser(user,userDetails){ // APPLY
         _changeUser.apply(user,userDetails);
     }
 
-    function addField(field,value){ //BIND
+    function addField(field,value){ // BINDED BELOW
         this[field] = value;
     }
 
@@ -57,7 +58,7 @@ myModule.printUser(user1);
 myModule.updateUser(user1,["Harshit", "Chhabra", 21]);
 myModule.printUser(user1);
 
-let myUpdater = myModule.addField.bind(user1);
+let myUpdater = myModule.addField.bind(user1); // BIND
 myUpdater("College", "Vasavi College of Engineering");
 myModule.printUser(user1);
 
